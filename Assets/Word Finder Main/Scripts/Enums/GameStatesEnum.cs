adc57c4aa10f0ffa609bbf47c73be42938b71da6
Shakur Farit <93837330+shakur-farit@users.noreply.github.com>
2023-06-06 18:36:15 +0400

    Game and Level_Complete Panel
    Create gameStates and GameManager to manage panels.

---
+++ Assets/Word Finder Main/Scripts/Enums/GameStatesEnum.cs
@@ -0,0 +1,9 @@
+public enum GameState
+{
+    None,
+    Menu,
+    Game,
+    LevelComplete,
+    GameOver,
+    Idle
+}
---
+++ Assets/Word Finder Main/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public static Action<GameState> onGameStateChanged;
+
+    private GameState gameState;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        gameState = GameState.None;
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+    }
+}
